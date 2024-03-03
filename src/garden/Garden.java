75b9106e47f2017aff9d6135f0062f8a072bafee
schuberte <schuberte@msoe.edu>
2024-03-03 13:45:31 -0600

    Added Bee, Flower, and Garden classes.

---
+++ src/garden/Garden.java
@@ -0,0 +1,13 @@
+package garden;
+
+import java.util.Set;
+
+public class Garden {
+    private Set<Flower> flowerBed;
+
+    private Set<Bee> beeHive;
+
+    public void step() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
