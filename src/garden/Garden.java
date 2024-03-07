aa3d9b6b5593deefb53c89f74bb451fc3c53083c
schuberte <schuberte@msoe.edu>
2024-03-07 12:01:27 -0600

    Moved flower instantiation logic from flower class to garden class

--- src/garden/Garden.java
+++ src/garden/Garden.java
@@ -1,11 +1,42 @@
 package garden;
 
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 
 public class Garden {
-    private Set<Flower> flowerBed;
+    private final Set<Flower> flowerBed;
 
     private Set<Bee> beeHive;
+
+
+    public Garden(Pane gardenPane) {
+        this.flowerBed= new HashSet<>();
+        for(int i = 0; i < 5; i++) {
+            NectarExchanger flowerNectar = i % 2 == 0? new BalancedNectar() : new NectarGiving();
+            flowerBed.add(new Flower(gardenPane, flowerNectar));
+        }
+    }
+
+    public Flower getNextFlowerTarget() {
+        int flower = new Random().nextInt(flowerBed.size());
+        int i = 0;
+        Flower target = (Flower) flowerBed.toArray()[0];
+        for(Flower f : flowerBed) {
+            if(i == flower) {
+                return f;
+            }
+            i++;
+        }
+        return target;
+    }
+
 
     public void step() {
         throw new UnsupportedOperationException("Not supported yet.");
